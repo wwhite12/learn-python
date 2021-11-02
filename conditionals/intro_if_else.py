439b782156d9878bf26aacacd7b3ca4813191411
William White <willcwhite@gmail.com>
2021-11-02 10:35:32 -0400

    conditionals

---
+++ conditionals/intro_if_else.py
@@ -0,0 +1,26 @@
+# Create a variable named `my_age` and assign it to your age
+
+
+# Create a variable named `darth_vader_age` and assign it to darth vaders age
+
+
+# Create an if statement to check if you are older than Darth Vader
+
+
+# Create a variable named `my_saber_color` and set it to any color
+
+
+# Create a variable named `vader_saber_color` and set it to red
+
+
+
+# Create an if statement if Darth Vader is older than you and if you both have the same color lightsaber
+
+
+
+# Create a variable named `hates_sand` and set it equal to the boolean that applies to Vader
+
+
+
+# We want to make an if statement that checks if you and Vader can be friends
+# write an if statement that checks if you and Vader have the same color lightsaber or if you hate sand
