5e463954f9360eb41fcfddf3c5b43ae464b0dfd5
William White <willcwhite@gmail.com>
2021-11-02 09:55:42 -0400

    variables intro, user input

---
+++ user_input/intro.py
@@ -0,0 +1,15 @@
+# Create a variable named `name` that accepts user input
+
+
+# print a statement that includes the users name and a compliment about them
+
+
+# create a variable named `max_bench` that accepts user input
+
+
+# print a statement that tells us what your max bench is
+
+
+# Create a new variable named `new_max` and set it to 50lbs over the user's max bench
+
+
---
+++ variables/intro.py
@@ -0,0 +1,39 @@
+"""Variables - the nouns of programming"""
+
+# This is a comment, any code in a comment will not be run
+
+# comments are very helpful to help you explain your code
+
+# TOPIC: strings
+
+# create a variable named `first_name` and give it the value of your name
+
+
+# create a variable named `last_name` and assign it a value of an empty string
+
+
+# create a variable name `full_name` and assign it the value of first_name and last_name concatenated together with a space between them
+
+
+
+# TOPIC: integers
+
+# create a variable named `age` and assign it the integer value of your age
+
+
+# create a variable named `years_of_lifting` and assign it the integer value of how many years you have been lifting
+
+
+# create a variable named `age_first_lifted` and assign it to the difference between your age and years_of_lifting
+
+
+
+# TOPIC: logging
+
+# print a statement that says `I love coding`
+
+
+# print the variable `full_name`
+
+
+# print a statement that tells us your full_name and how old you were when you started lifting
